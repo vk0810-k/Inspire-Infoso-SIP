de0a700c5e2911717046beed20b4e2b4241e9804
vk0810-k <varunkhadayate0810@gmail.com>
2022-06-15 08:58:27 +0530

    Flutter TIC TAC TOE Project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_tic_tac_toe/main.dart';
+import 'package:flutter_tic_tac_toe_varun_khadayate/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
